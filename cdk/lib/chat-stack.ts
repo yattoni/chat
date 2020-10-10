5fd66800b3485ca617c252c5205947d7be81bef8
Austin Yattoni <austinyattoni@gmail.com>
2020-10-10 13:32:29 -0500

    reformat with prettier

--- cdk/lib/chat-stack.ts
+++ cdk/lib/chat-stack.ts
@@ -3,13 +3,12 @@ import * as s3 from '@aws-cdk/aws-s3';
 import * as lambda from '@aws-cdk/aws-lambda';
 
 export class ChatStack extends core.Stack {
-
   constructor(scope: core.Construct, id: string, props?: core.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
     new s3.Bucket(this, 'Bucket', {
-      removalPolicy: core.RemovalPolicy.DESTROY
+      removalPolicy: core.RemovalPolicy.DESTROY,
     });
 
     new lambda.Function(this, 'Lambda', {
@@ -19,12 +18,12 @@ export class ChatStack extends core.Stack {
           command: [
             'sh',
             '-c',
-            'export GOBIN=$(go env GOPATH)/bin && go get -t ./... && go test . && go build -o /asset-output/main'
-          ]
-        }
+            'export GOBIN=$(go env GOPATH)/bin && go get -t ./... && go test . && go build -o /asset-output/main',
+          ],
+        },
       }),
       handler: 'main',
-      runtime: lambda.Runtime.GO_1_X
-    })
+      runtime: lambda.Runtime.GO_1_X,
+    });
   }
 }
